09456741ff527cea08008b7b76675389c1c621cf
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-04 13:52:10 -0300

    att

---
+++ JavaScriptBasic/consolelog.js
@@ -0,0 +1,8 @@
+console.log('Ednardo'); // String = TEXTO
+console.log("Ednardo");
+console.log(`Ednardo`);
+
+
+console.log(1); // NUMBER, EM JS NAO TEM SEPARACAO DE INT E FLOAT
+
+console.log(3,144444444); // NUMBER, EM JS NAO TEM SEPARACAO DE INT E FLOAT
---
+++ JavaScriptBasic/ex01/exercicio1.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Ednardo". Estou aprendendo JavaScript as', 13,'da tarde.')
