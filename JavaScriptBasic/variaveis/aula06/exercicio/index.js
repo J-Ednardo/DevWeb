77cf9a6fda9d412f3a5bcd41b574490f1b65dc1d
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-05 12:35:31 -0300

    att

---
+++ JavaScriptBasic/variaveis/aula06/exercicio/index.js
@@ -0,0 +1,9 @@
+const nome = 'Jose Ednardo'
+const sobrenome = 'Dantas Neto'
+let idade = 20
+let pesoEmKg = 70
+let alturaEmM = 1.8
+let imc = pesoEmKg / (alturaEmM ** 2)
+let anoNascimento = 2025 - idade
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${pesoEmKg}kg tem ${alturaEmM} de altura e seu IMC e de ${imc}`)
+console.log(nome + ' nasceu em ' + anoNascimento)
