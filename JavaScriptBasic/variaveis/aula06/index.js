fa39ea7bae97b82cc5b81d21687d626bb9cc13b7
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-05 12:20:28 -0300

    att

---
+++ JavaScriptBasic/variaveis/aula06/index.js
@@ -0,0 +1,14 @@
+// Nao podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+// Nao pode comecaro o nome de uma variavel com um numero
+// Nao pode conter espacos ou tracos
+// Utilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveis com let
+
+
+let nome; //declarando variavel
+nome = 'Ednardo'; //Inicializando a variavel
+console.log(nome);
+nome = 'Jose';
+console.log(nome);
