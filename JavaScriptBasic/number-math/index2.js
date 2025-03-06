50935235fcdbba3e4b7ef8651af9493c1915c148
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-06 12:21:23 -0300

    Create index2.js

---
+++ JavaScriptBasic/number-math/index2.js
@@ -0,0 +1,7 @@
+let num1 = 9.51235
+//let num2 = Math.floor(num1) arredonda para baixo
+//let num2 = Math.ceil(num1) arredonda para cima
+//let num2 = Math.round(num1) arredonda para o mais proximo
+const aleatorio = Math.round(Math.random() *  (10-5) + 5)
+
+console.log(aleatorio)
