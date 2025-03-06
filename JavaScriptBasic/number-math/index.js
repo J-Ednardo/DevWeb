c817b9174c329c25f15a855c6bbcc8ad93e39ac4
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-06 11:43:35 -0300

    Create index.js

---
+++ JavaScriptBasic/number-math/index.js
@@ -0,0 +1,11 @@
+let num1 = 15.91238123
+let num2 = 25
+
+
+//console.log(num1.toString() + num2)
+//num1 = num1.toString()
+//console.log(num1.toString(2))
+//console.log(num1.toFixed(2))
+//console.log(Number.isInteger(num2))
+//let temp = num1 * 'Ola'
+//console.log(Number.isNaN(temp))
