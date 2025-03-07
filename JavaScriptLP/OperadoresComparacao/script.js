2b9a4d9635f8f868cf70caa98625bd5115f4e112
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-07 11:42:10 -0300

    Create script.js

---
+++ JavaScriptLP/OperadoresComparacao/script.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparacao
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a 
+== igualdade (valor) ****
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ****
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10
+const num2 = 11
+const num3 = 9
+const num4 = num1
+
+console.log(num1 > num3)
+console.log(num2 < num1)
+console.log(num1 === num4)
