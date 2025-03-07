3691cb064eb715206907b20a2fa8a726517e8910
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-07 10:17:12 -0300

    function

---
+++ JavaScriptBasic/function/index.js
@@ -0,0 +1,25 @@
+function soma(x =0 , y = 0)  {
+    const resultado = x + y;
+    return resultado;
+}
+
+function subtracao(x = 0,y = 0) {
+    const resultado = x - y;
+    return resultado;
+}
+
+function multiplicacao(x = 0, y = 0) {
+    const resultado = x * y;
+    return resultado;
+}
+
+function divisao(x = 0, y = 0) {
+    const resultado = x / y ;
+    return resultado;
+}
+
+
+const raizQuadrada = n =>  Math.sqrt(n);
+
+
+console.log(raizQuadrada(9))
