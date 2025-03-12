d4ab024e3bb1c27cd1cb9e53a51761a33f12eaa2
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-03-12 00:02:05 -0300

    cmt

---
+++ JavaScriptLP/EstruturasDeRepeticao/For/index.js
@@ -0,0 +1,16 @@
+// for (variave; condicao; incremento/decremento) {bloco de codigo}
+
+const frutas = ['Maca', 'Pera', 'Uvas']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+
+/*for (let i = 1; i <= 10; i++) {
+    const par = i % 2 === 0
+    if (par) {
+        console.log(`O numero ${i} eh par`)
+    } else {
+        console.log(`O numero ${i} eh impar`)
+    }
+}*/
